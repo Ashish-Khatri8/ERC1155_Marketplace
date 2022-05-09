f117fba1f443505b11ff14cb3908c4f4a9d55f53
Ashish Khatri <khatri8.ashish@gmail.com>
2022-05-09 15:15:56 +0530

    Added Tokens contract to mint ERC1155 tokens.

---
+++ contracts/Tokens.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract Tokens is ERC1155 {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    uint256 private constant BlazeToken = 0;
+
+    mapping(address => bool) private hasClaimedTokens;
+
+    constructor(
+        string memory _uriLink,
+        uint256 _tokenInitialSupply
+    ) ERC1155(_uriLink) {
+        _mint(msg.sender, BlazeToken, _tokenInitialSupply, "");
+    }
+
+    function claimBlazeTokens() external {
+        require(
+            hasClaimedTokens[msg.sender] == false,
+            "Tokens: You have already claimed your share of tokens."
+        );
+        _mint(msg.sender, BlazeToken, 50000, "");
+        hasClaimedTokens[msg.sender] = true;
+    }
+
+    function mintNFT(uint256 _copies) external {
+        require(
+            _copies > 0 && _copies < 6,
+            "Tokens: Can mint copies between 1 and 5 only.!"
+        );
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _mint(msg.sender, newTokenId, _copies, "");
+    }
+}
